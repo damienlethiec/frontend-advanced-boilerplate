ffa76442581b7dd170e0f456ef6d8257c5c52b00
Damien Le Thiec <damien.lethiec@gmail.com>
2016-08-01 17:26:40 +0200

    Fin pages

--- config.rb
+++ config.rb
@@ -89,3 +89,7 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+data.flats.each_key do |owner|
+  proxy "/flats/#{owner}.html", "show.html", :locals => { :owner => owner }, :ignore => true
+end
